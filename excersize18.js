e1228e035c2a524586c21275a5986260609854b9
Rey <ray.greenm@gmail.com>
2018-07-31 14:25:36 -0500

    Rey

---
+++ excersize18.js
@@ -0,0 +1,13 @@
+function main(){
+    var num1 = 10;
+    var num2 = 20;
+    alert(product(num1,num2));
+    alert(quotient(num1,num2))
+}
+function product(first, second){
+    return (first * second)
+}
+function quotient(first, second){
+    return (first / second)
+}
+ main();
